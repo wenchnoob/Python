15a4791f4f65cad027651cd3b55f636dd7db851f
Wenchy Dutreuil <wcdutreuil@gmail.com>
2020-11-04 13:28:16 -0600

    did FizzBuzz with list comprehensions in python -- improved the list comprehension function

--- MyOwnPractice/BinaryToDecimal.py
+++ MyOwnPractice/BinaryToDecimal.py
@@ -6,14 +6,16 @@ def binary_to_decimal(binary: str):
 
 
 def binary_to_decimal_list_comprehension(binary: str):
-    ls = [(binary.index(i) + 1) * 2 ** (binary.index(i)) for i in binary[::-1]]
-    return sum(ls)
+    return sum([int(binary[::-1][i]) * (2 ** i) for i in range(len(binary))])
 
 
 five = binary_to_decimal("101")  # should be 5
 twelve = binary_to_decimal("1100")  # should be 12
 
-ten = binary_to_decimal_list_comprehension("1010")
+ten = binary_to_decimal_list_comprehension("1010")  # should be 10
+twelve_with_list_comprehension = binary_to_decimal_list_comprehension("1100")  # should be 12
+five_with_list_comprehension = binary_to_decimal_list_comprehension("101")  # should be 5
+
+print(five, twelve, ten, twelve_with_list_comprehension, five_with_list_comprehension)
 
 
-print(five, twelve, ten)
